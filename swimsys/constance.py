c14f8d6802ce4729b6e380979a4e767ba0ef6721
fangxin-tang <susantang.nz@gmail.com>
2023-09-17 22:05:17 +1200

    Create constance.py

---
+++ swimsys/constance.py
@@ -0,0 +1,31 @@
+COURSE_TYPES = [
+    'Aqua Aerobics',
+    'Aqua Cycling',
+    'Aqua Yoga',
+    'Aqua Fitness',
+    'Aqua Pilates'
+    ]
+
+COURSE_NAMES = [
+    'Aqua Fit',
+    'Water Zumba',
+    'Deep Water Workout',
+    'Cardio Splash',
+    'Aqua Strength',
+    'Hydro Cycling',
+    'Aqua Yoga',
+    'Swim-Sculpt',
+    'Aquatic Bootcamp',
+    'Poolside Pilates'
+    ]
+
+TIME_SLOTS = [
+    (9, 10), (10, 11), (11, 12),
+    (12, 13), (13, 14), (14, 15),
+    (15, 16), (16, 17), (17, 18), (18, 19)
+]
+
+
+
+
+DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
